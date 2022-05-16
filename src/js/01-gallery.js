ab7bf6768294ba6ba3a75ee8f341fbcd00e73607
OlegMishko <oleg.mishko@gmail.com>
2022-05-16 22:03:06 +0300

    js-hw-v1.1
    выполнено задание 1:

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+let markupImg = "";
+const refs = { imageGallery: document.querySelector('.gallery'), };
+
+galleryItems.forEach(({ preview, original, description }) => {
+
+    markupImg += `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+    </div>
+    `;
+
+});
+
+addImgMarkup(markupImg);
+
+function addImgMarkup(markup) {
+    refs.imageGallery.insertAdjacentHTML('beforeend', markup);
+    console.log(markup);
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+
+    captionsData: "alt",
+    captionDelay: 250,
+});
