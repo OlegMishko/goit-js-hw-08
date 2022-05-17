1a43d2864cab83d7c90d13913804a04e01e5dd3e
OlegMishko <oleg.mishko@gmail.com>
2022-05-17 22:55:16 +0300

    js-hw-v1.2
    Добавлено работающие варианты Video и Feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const LOCALFORM_STATE = "feedback-form-state";
 
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormImput, 500));
+
+onFormShow();
+function onFormSubmit(e) {
+    e.preventDefault();
+    if (form.elements.email.value && form.elements.message.value) {
+        console.log(onObjectSet())
+        e.currentTarget.reset();
+        localStorage.removeItem(LOCALFORM_STATE);
+    }
+};
+
+function onObjectSet() {
+    return {
+        email: form.elements.email.value,
+        message: form.elements.message.value,
+    };
+};
+
+function onFormImput() {
+    const iputsFields = onObjectSet();
+    localStorage.setItem(LOCALFORM_STATE, JSON.stringify(iputsFields));
+};
+
+
+function onFormShow() {
+    const savedMessage = localStorage.getItem(LOCALFORM_STATE)
+    if (savedMessage) {
+        const inputFill = JSON.parse(savedMessage);
+        form.elements.email.value = inputFill.email;
+        form.elements.message.value = inputFill.message;
+    }
+
+};
